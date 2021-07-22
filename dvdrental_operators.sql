4ef78b7a8086df2c210746f13ca90892778d20b2
SultanDogan <68539268+SultanDogan@users.noreply.github.com>
2021-07-22 17:04:02 +0300

    Update dvdrental_operators.sql

--- dvdrental_operators.sql
+++ dvdrental_operators.sql
@@ -18,3 +18,16 @@ SELECT * FROM film
 WHERE NOT length > 50 AND NOT (rental_rate = 2.99 OR rental_rate = 4.99);
 
 
+-- Take the films that replacement cost is between 12.99 and 16.99.
+SELECT * FROM film
+WHERE replacement_cost BETWEEN 12.99 AND 16.99;
+
+--From actor table take the first and last name of actors that first names are Penelope, Nick or Ed.
+SELECT first_name, last_name FROM actor
+WHERE first_name IN ('Penelope','Nick','Ed');
+
+--From film table take the films which have 0.99 , 2.99 or 4.99 as a rental rate and 12.99 , 15.99 or 28.99 as a replacement cost.
+SELECT * FROM film
+WHERE rental_rate IN (0.99,2.99,4.99) AND replacement_cost IN (12.99,15.99,28.99);
+
+
